aa6a64e18cb564c2cac1e8438d3daef456ca6ebe
LevelUpMarketers <admin@leveluprichmond.com>
2026-03-18 18:19:17 -0400

    Normalize dashboard profile state values

--- includes/shortcodes/class-teqcidb-shortcode-student-dashboard.php
+++ includes/shortcodes/class-teqcidb-shortcode-student-dashboard.php
@@ -2232,7 +2232,86 @@ class TEQCIDB_Shortcode_Student_Dashboard {
             $defaults['postal_code'] = sanitize_text_field( (string) $decoded['zip_code'] );
         }
 
+        $defaults['state'] = $this->normalize_dashboard_state_value( $defaults['state'] );
+
         return $defaults;
+    }
+
+    private function normalize_dashboard_state_value( $value ) {
+        $normalized = sanitize_text_field( (string) $value );
+
+        if ( '' === $normalized ) {
+            return '';
+        }
+
+        $states = $this->get_dashboard_state_options();
+
+        foreach ( $states as $state ) {
+            if ( 0 === strcasecmp( $normalized, $state ) ) {
+                return $state;
+            }
+        }
+
+        $state_codes = array(
+            'AL' => 'Alabama',
+            'AK' => 'Alaska',
+            'AZ' => 'Arizona',
+            'AR' => 'Arkansas',
+            'CA' => 'California',
+            'CO' => 'Colorado',
+            'CT' => 'Connecticut',
+            'DE' => 'Delaware',
+            'FL' => 'Florida',
+            'GA' => 'Georgia',
+            'HI' => 'Hawaii',
+            'ID' => 'Idaho',
+            'IL' => 'Illinois',
+            'IN' => 'Indiana',
+            'IA' => 'Iowa',
+            'KS' => 'Kansas',
+            'KY' => 'Kentucky',
+            'LA' => 'Louisiana',
+            'ME' => 'Maine',
+            'MD' => 'Maryland',
+            'MA' => 'Massachusetts',
+            'MI' => 'Michigan',
+            'MN' => 'Minnesota',
+            'MS' => 'Mississippi',
+            'MO' => 'Missouri',
+            'MT' => 'Montana',
+            'NE' => 'Nebraska',
+            'NV' => 'Nevada',
+            'NH' => 'New Hampshire',
+            'NJ' => 'New Jersey',
+            'NM' => 'New Mexico',
+            'NY' => 'New York',
+            'NC' => 'North Carolina',
+            'ND' => 'North Dakota',
+            'OH' => 'Ohio',
+            'OK' => 'Oklahoma',
+            'OR' => 'Oregon',
+            'PA' => 'Pennsylvania',
+            'RI' => 'Rhode Island',
+            'SC' => 'South Carolina',
+            'SD' => 'South Dakota',
+            'TN' => 'Tennessee',
+            'TX' => 'Texas',
+            'UT' => 'Utah',
+            'VT' => 'Vermont',
+            'VA' => 'Virginia',
+            'WA' => 'Washington',
+            'WV' => 'West Virginia',
+            'WI' => 'Wisconsin',
+            'WY' => 'Wyoming',
+        );
+
+        $upper_code = strtoupper( $normalized );
+
+        if ( isset( $state_codes[ $upper_code ] ) ) {
+            return $state_codes[ $upper_code ];
+        }
+
+        return $normalized;
     }
 
     private function decode_representative_field( $value ) {
